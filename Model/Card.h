32c3f37855f534e30df640938d38a5978fc3a1a4
sidens <sidens@gmail.com>
2013-12-08 14:53:10 -0800

    Adding code from lecture 2. Still needs models from lectures 1&2

---
+++ Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Machismo
+//
+//  Created by Steve Siden on 12/8/13.
+//  Copyright (c) 2013 Steve Siden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Machismo
+//
+//  Created by Steve Siden on 12/8/13.
+//  Copyright (c) 2013 Steve Siden. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
