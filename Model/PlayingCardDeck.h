63260ca02c3e05c507dbd0dd497a7baae67eaffa
sidens <sidens@gmail.com>
2013-12-25 21:44:46 -0500

    Added PlayingCardDeck[mh]

---
+++ Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Machismo
+//
+//  Created by Steve Siden on 12/25/13.
+//  Copyright (c) 2013 Steve Siden. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
