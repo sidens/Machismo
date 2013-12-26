6b0eca53001f269343aef5bd89321e317a73dd95
sidens <sidens@gmail.com>
2013-12-25 22:29:48 -0500

    Adding code for Assignment 1

--- Machismo/CardGameViewController.m
+++ Machismo/CardGameViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (nonatomic) Deck * deck;
 @end
 
 @implementation CardGameViewController
@@ -24,20 +26,33 @@
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
-    if( [sender.currentTitle length] )
+    while (self.flipCount<52)
     {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];
+        if( [sender.currentTitle length] ) //Show the back of the card
+        {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:@"" forState:UIControlStateNormal];
+        }
+        else //Show the front of the card
+        {
+            Card *card = [self.deck drawRandomCard]; //Draw random card
+            
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:[card contents] forState:UIControlStateNormal];
+        }
+        self.flipCount++;
     }
-    else
-    {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
-    }
-    self.flipCount++;
+    
 }
+
+-(Deck *) deck{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+
 
 
 @end
